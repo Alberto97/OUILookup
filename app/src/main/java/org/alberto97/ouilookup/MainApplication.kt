7cb35b75a4ede8933c1efb89a328441d0b60c221
Alberto Pedron <albertop2197@gmail.com>
2021-08-02 19:08:44 +0200

    Project setup for WorkManager with Hilt

--- app/src/main/java/org/alberto97/ouilookup/MainApplication.kt
+++ app/src/main/java/org/alberto97/ouilookup/MainApplication.kt
@@ -1,7 +1,19 @@
 package org.alberto97.ouilookup
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
