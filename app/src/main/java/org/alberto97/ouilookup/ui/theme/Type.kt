d56567df7a7d966fe56516f0850dae1a0d65354c
Alberto Pedron <albertop2197@gmail.com>
2021-09-23 15:10:02 +0200

    Import White Rabbit font

--- app/src/main/java/org/alberto97/ouilookup/ui/theme/Type.kt
+++ app/src/main/java/org/alberto97/ouilookup/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package org.alberto97.ouilookup.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.alberto97.ouilookup.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -25,4 +27,8 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+val whiteRabbitFamily = FontFamily(
+    Font(R.font.whitrabt)
 )
