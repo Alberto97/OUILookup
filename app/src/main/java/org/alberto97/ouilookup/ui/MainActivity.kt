19ae435e67ebfceb56120148275b48640906435e
Alberto Pedron <albertop2197@gmail.com>
2021-10-12 15:16:46 +0200

    Add splash screen for Android 12

--- app/src/main/java/org/alberto97/ouilookup/ui/MainActivity.kt
+++ app/src/main/java/org/alberto97/ouilookup/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
+import org.alberto97.ouilookup.R
 import org.alberto97.ouilookup.ui.theme.OUILookupTheme
 
 @AndroidEntryPoint
@@ -11,6 +12,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.Theme_OUILookup_NoActionBar)
+
         setContent {
             OUILookupTheme {
                 NavGraph()
