30e089721bccb6a80ae84917a52da75a50d5a893
Alberto Pedron <albertop2197@gmail.com>
2022-02-01 20:16:13 +0100

    Use stable navigation-compose and hilt-navigation-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,8 +89,8 @@ dependencies {
     kapt("androidx.hilt:hilt-compiler:1.0.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.4.0-beta01")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
+    implementation("androidx.navigation:navigation-compose:2.4.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // Retrofit
     val retrofitVersion = "2.9.0"
