21be33bb13fc8d2670da48df28f58fa8840ce2ec
Alberto Pedron <albertop2197@gmail.com>
2022-02-02 15:47:38 +0100

    ui: search: Implement collapsible toolbar with persistent searchbar

--- app/src/main/java/org/alberto97/ouilookup/ui/search/SearchScreen.kt
+++ app/src/main/java/org/alberto97/ouilookup/ui/search/SearchScreen.kt
@@ -1,10 +1,6 @@
 package org.alberto97.ouilookup.ui.search
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,10 +11,16 @@ import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.SearchOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.alberto97.ouilookup.R
@@ -26,6 +28,7 @@ import org.alberto97.ouilookup.db.Oui
 import org.alberto97.ouilookup.ui.Destinations
 import org.alberto97.ouilookup.ui.FullscreenPlaceholder
 import org.alberto97.ouilookup.ui.theme.OUILookupTheme
+import kotlin.math.roundToInt
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -55,19 +58,59 @@ fun SearchScreen(
     list: List<Oui>,
     updatingDb: Boolean
 ) {
+    val toolbarHeight = 48.dp
+    val toolbarHeightPx = with(LocalDensity.current) { toolbarHeight.roundToPx().toFloat() }
+    val toolbarOffsetHeightPx = remember { mutableStateOf(0f) }
+
+    val searchbarHeight = 72.dp
+    val searchbarTopPaddingPx = remember { mutableStateOf(toolbarHeightPx) }
+
+    // Create connection to the nested scroll system and listen to the scroll
+    // happening inside child LazyColumn
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                // try to consume before LazyColumn to collapse toolbar if needed, hence pre-scroll
+                val delta = available.y
+
+                val newOffset = toolbarOffsetHeightPx.value + delta
+                toolbarOffsetHeightPx.value = newOffset.coerceIn(-toolbarHeightPx, 0f)
+
+                val newPadding = searchbarTopPaddingPx.value + delta
+                searchbarTopPaddingPx.value = newPadding.coerceIn(0f, toolbarHeightPx)
+
+                // here's the catch: let's pretend we consumed 0 in any case, since we want
+                // LazyColumn to scroll anyway for good UX
+                // We're basically watching scroll without taking it
+                return Offset.Zero
+            }
+        }
+    }
 
     Scaffold {
-        Column(
-            modifier = Modifier.background(MaterialTheme.colors.primarySurface)
+        Box(
+            Modifier
+                .fillMaxSize()
+                // attach as a parent to the nested scroll system
+                .nestedScroll(nestedScrollConnection)
         ) {
-            SearchBar(
-                onInfoClick = onInfoClick,
-                text = text,
-                onTextChange = onTextChange
-            )
             Content(
                 list = list,
-                updatingDb = updatingDb
+                updatingDb = updatingDb,
+                listContentTopPadding = toolbarHeight + searchbarHeight,
+            )
+            SearchBar(
+                text = text,
+                onTextChange = onTextChange,
+                searchbarTopPadding = with(LocalDensity.current) { searchbarTopPaddingPx.value.toDp() }
+            )
+            TopAppBar(
+                title = { Text(stringResource(R.string.app_name)) },
+                elevation = 0.dp,
+                actions = { DropdownButton(onInfoClick) },
+                modifier = Modifier
+                    .height(toolbarHeight)
+                    .offset { IntOffset(x = 0, y = toolbarOffsetHeightPx.value.roundToInt()) },
             )
         }
     }
@@ -105,21 +148,20 @@ fun Dropdown(
 
 @Composable
 fun SearchBar(
-    onInfoClick: () -> Unit,
     text: String,
     onTextChange: (value: String) -> Unit,
+    searchbarTopPadding: Dp,
 ) {
-    Column(
-        modifier = Modifier.fillMaxWidth()
+    Surface(
+        color = MaterialTheme.colors.primarySurface,
+        elevation = AppBarDefaults.TopAppBarElevation,
+        modifier = Modifier.padding(top = searchbarTopPadding)
     ) {
         TextField(
             value = text,
             placeholder = { Text(stringResource(R.string.search_text_field_placeholder)) },
             onValueChange = onTextChange,
-            leadingIcon = {
-                Icon(Icons.Outlined.Search, null)
-            },
-            trailingIcon = { DropdownButton(onInfoClick) },
+            leadingIcon = { Icon(Icons.Outlined.Search, null) },
             shape = RoundedCornerShape(12.dp),
             colors = TextFieldDefaults.textFieldColors(
                 focusedIndicatorColor = Color.Transparent,
@@ -129,7 +171,6 @@ fun SearchBar(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(14.dp)
-                .shadow(8.dp)
         )
     }
 }
@@ -138,34 +179,29 @@ fun SearchBar(
 @Composable
 private fun Content(
     list: List<Oui>,
-    updatingDb: Boolean
+    updatingDb: Boolean,
+    listContentTopPadding: Dp
 ) {
-    Surface(
-        shape = RoundedCornerShape(
-            topStart = 12.dp,
-            topEnd = 12.dp
-        )
-    ) {
-        if (updatingDb)
-            FullscreenPlaceholder(
-                text = stringResource(R.string.search_update_database),
-            ) {
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .size(62.dp)
-                        .padding(8.dp)
-                )
-            }
-        else
-            Items(
-                list = list
+    if (updatingDb)
+        FullscreenPlaceholder(
+            text = stringResource(R.string.search_update_database),
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(62.dp)
+                    .padding(8.dp)
             )
-    }
+        }
+    else
+        Items(
+            list = list,
+            listContentTopPadding
+        )
 }
 
 @ExperimentalMaterialApi
 @Composable
-fun Items(list: List<Oui>) {
+fun Items(list: List<Oui>, listContentTopPadding: Dp) {
     if (list.isEmpty())
         FullscreenPlaceholder(
             text = stringResource(R.string.search_results_not_found),
@@ -173,6 +209,7 @@ fun Items(list: List<Oui>) {
         )
     else
         LazyColumn(
+            contentPadding = PaddingValues(top = listContentTopPadding),
             modifier = Modifier.padding(12.dp)
         ) {
             items(list) { device ->
