91faaa5089c7e0f7751f89d79bc9064d3a6e6ebc
Alberto Pedron <albertop2197@gmail.com>
2022-02-03 15:19:19 +0100

    DownloadWorker: Print stacktrace to logcat in case of failure

--- app/src/main/java/org/alberto97/ouilookup/workers/DownloadWorker.kt
+++ app/src/main/java/org/alberto97/ouilookup/workers/DownloadWorker.kt
@@ -6,6 +6,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.hilt.work.HiltWorker
@@ -31,6 +32,7 @@ class DownloadWorker  @AssistedInject constructor(
         try {
             repository.updateIfOldOrEmpty()
         } catch (e: Exception) {
+            Log.e("OUILookup", e.stackTraceToString())
             return Result.failure()
         }
 
