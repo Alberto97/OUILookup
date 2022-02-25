48abb2a1e6ba5c20361a9442a3ceb4a7420bd199
Alberto Pedron <albertop2197@gmail.com>
2022-02-25 15:11:38 +0100

    Define Android Gradle Plugin version in a variable
    That will make dependabot open only one pull request

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.1.2" apply false
-    id("com.android.library") version "7.1.2" apply false
+    val agpVersion = "7.1.2"
+    id("com.android.application") version agpVersion apply false
+    id("com.android.library") version agpVersion apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
     id("com.google.dagger.hilt.android") version "2.41" apply false
 }
