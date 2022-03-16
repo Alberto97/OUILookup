c8cf5c6bd8724907e05e29c7bf7c5f625ecb0e23
Alberto Pedron <albertop2197@gmail.com>
2022-03-16 14:01:06 +0100

    Add Accompanist Navigation-Material
    I'm not thrilled of depending on a library whose only purpose is to be later deprecated though

--- app/src/main/java/org/alberto97/ouilookup/ui/NavGraph.kt
+++ app/src/main/java/org/alberto97/ouilookup/ui/NavGraph.kt
@@ -5,6 +5,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.ModalBottomSheetLayout
+import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 import org.alberto97.ouilookup.ui.about.AboutScreen
 import org.alberto97.ouilookup.ui.about.AboutViewModel
 import org.alberto97.ouilookup.ui.search.SearchScreen
@@ -15,18 +18,22 @@ object Destinations {
     const val ABOUT_ROUTE = "about"
 }
 
+@OptIn(ExperimentalMaterialNavigationApi::class)
 @Composable
 fun NavGraph() {
-    val navController = rememberNavController()
+    val bottomSheetNavigator = rememberBottomSheetNavigator()
+    val navController = rememberNavController(bottomSheetNavigator)
 
-    NavHost(navController, startDestination = Destinations.SEARCH_ROUTE) {
-        composable(Destinations.SEARCH_ROUTE) {
-            val viewModel: SearchViewModel = hiltViewModel(it)
-            SearchScreen(viewModel, navController)
-        }
-        composable(Destinations.ABOUT_ROUTE) {
-            val viewModel: AboutViewModel = hiltViewModel(it)
-            AboutScreen(viewModel, navController)
+    ModalBottomSheetLayout(bottomSheetNavigator) {
+        NavHost(navController, startDestination = Destinations.SEARCH_ROUTE) {
+            composable(Destinations.SEARCH_ROUTE) {
+                val viewModel: SearchViewModel = hiltViewModel(it)
+                SearchScreen(viewModel, navController)
+            }
+            composable(Destinations.ABOUT_ROUTE) {
+                val viewModel: AboutViewModel = hiltViewModel(it)
+                AboutScreen(viewModel, navController)
+            }
         }
     }
 }
