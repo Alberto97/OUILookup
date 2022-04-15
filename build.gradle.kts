961626f3f44ca4ef454b0c55d198d6801b64ad33
Alberto Pedron <albertop2197@gmail.com>
2022-04-15 16:31:00 +0200

    Upgrade AGP to v7.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val agpVersion = "7.1.2"
+    val agpVersion = "7.1.3"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
