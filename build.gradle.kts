21e1a992c1371c97ab2935923368c2354f4ceb97
Alberto Pedron <albertop2197@gmail.com>
2022-07-09 22:14:55 +0200

    Upgrade Kotlin to v1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     val agpVersion = "7.2.1"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
-    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
     id("com.google.dagger.hilt.android") version "2.42" apply false
 }
 
