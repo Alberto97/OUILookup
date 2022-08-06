e948ea4e3ba040e90d3e6097ccd1614d451b9af6
Alberto Pedron <albertop2197@gmail.com>
2022-08-06 18:24:55 +0200

    Bump AGP from 7.2.1 to 7.2.2 (#81)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val agpVersion = "7.2.1"
+    val agpVersion = "7.2.2"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
     id("org.jetbrains.kotlin.android") version "1.7.0" apply false
