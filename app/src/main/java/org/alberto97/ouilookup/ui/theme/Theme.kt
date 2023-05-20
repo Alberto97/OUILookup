c050d9b0e7969ff7a2f4cfc95fa925e4661e5760
Alberto Pedron <albertop2197@gmail.com>
2023-05-20 18:37:29 +0200

    Use a Navigation Bar instead of Toolbar actions

--- app/src/main/java/org/alberto97/ouilookup/ui/theme/Theme.kt
+++ app/src/main/java/org/alberto97/ouilookup/ui/theme/Theme.kt
@@ -7,20 +7,25 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Blue200,
-    secondary = Amber200,
-    tertiary = Blue700
+    secondary = Blue700,
+    tertiary = Blue700,
+    secondaryContainer = Color(0xFF3b4858),
+    onSecondaryContainer = Color(0xFFd0e4ff)
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Blue500,
-    secondary = Amber200,
-    tertiary = Blue700
+    secondary = Blue500,
+    tertiary = Blue200,
+    secondaryContainer = Color(0xFFd1e4ff),
+    onSecondaryContainer = Color(0xFF001d34)
 )
 
 @Composable
