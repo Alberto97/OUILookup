dccf0f52ba2f4a8193eeb83ec9ba28c40d2d58f4
Alberto Pedron <albertop2197@gmail.com>
2023-09-02 20:43:27 +0200

    build: Setup KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
     id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
     id("org.gradle.android.cache-fix")
 }
 
