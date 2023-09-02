2a374eba9a22b0cf5069607a034173b26450f078
Alberto Pedron <albertop2197@gmail.com>
2023-09-02 20:43:27 +0200

    build: Setup KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val agpVersion = "8.1.1"
+    val kotlinVersion = "1.8.22"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.11" apply false
     id("org.gradle.android.cache-fix") version "2.7.4" apply false
 }
 
