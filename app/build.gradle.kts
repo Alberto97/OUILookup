5963f721f7d4fb0f2ad7a67942a5cb9c0057f07f
Alberto Pedron <albertop2197@gmail.com>
2023-11-17 17:38:52 +0100

    app: Update Compose Compiler to v1.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
 
     room {
