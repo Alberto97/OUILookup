6d20e66eed4d1dd2b80a850b5b835a7ea2a97def
Alberto Pedron <albertop2197@gmail.com>
2023-12-01 16:09:57 +0100

    Rework WorkManager initialization

--- app/src/main/java/org/alberto97/ouilookup/MainApplication.kt
+++ app/src/main/java/org/alberto97/ouilookup/MainApplication.kt
@@ -1,19 +1,7 @@
 package org.alberto97.ouilookup
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    override fun getWorkManagerConfiguration() =
-        Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-}
+class MainApplication : Application()
