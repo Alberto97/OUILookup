b2340e21508640dfe052eb17ada46ac429eb948a
Alberto Pedron <albertop2197@gmail.com>
2024-05-19 20:16:33 +0200

    AppConnectivityManager: Remove code to support legacy SDKs

--- app/src/main/java/org/alberto97/ouilookup/tools/AppConnectivityManager.kt
+++ app/src/main/java/org/alberto97/ouilookup/tools/AppConnectivityManager.kt
@@ -3,8 +3,6 @@ package org.alberto97.ouilookup.tools
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Build
-import androidx.annotation.RequiresApi
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -20,26 +18,7 @@ class AppConnectivityManager @Inject constructor(
 
     override fun isConnected(): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-            isConnected(cm)
-        else
-            isConnectedLegacy(cm)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun isConnected(cm: ConnectivityManager): Boolean {
         val cap = cm.getNetworkCapabilities(cm.activeNetwork) ?: return false
         return cap.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-    }
-
-    @Suppress("DEPRECATION")
-    private fun isConnectedLegacy(cm: ConnectivityManager): Boolean {
-        val networks = cm.allNetworks
-        for (n in networks) {
-            val nInfo = cm.getNetworkInfo(n)
-            if (nInfo != null && nInfo.isConnected) return true
-        }
-
-        return false
     }
 }
