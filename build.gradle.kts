ba134fc794177f30980bc166260a616e3ae8ae4d
Alberto Pedron <albertop2197@gmail.com>
2024-05-19 17:34:33 +0200

    Update KSP to v1.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.android.library") version agpVersion apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
-    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.17" apply false
+    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.20" apply false
     id("org.gradle.android.cache-fix") version "3.0.1" apply false
 }
 
