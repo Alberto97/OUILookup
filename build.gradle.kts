333b7b225f07b0d44e58b35403cf868618773045
Alberto Pedron <albertop2197@gmail.com>
2024-05-22 20:49:53 +0200

    Upgrade to Kotlin 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val agpVersion = "8.4.1"
-    val kotlinVersion = "1.9.24"
+    val kotlinVersion = "2.0.0"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
+    id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.devtools.ksp") version "$kotlinVersion-1.0.20" apply false
     id("org.gradle.android.cache-fix") version "3.0.1" apply false
