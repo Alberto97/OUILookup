d79e91239e3caa31cd4d99bc75bece6c8e0ddd8f
Alberto Pedron <albertop2197@gmail.com>
2025-05-16 23:03:22 +0200

    Do not use no more needed desugaring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,6 @@ android {
     }
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -93,8 +92,6 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
-
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.10.0")
