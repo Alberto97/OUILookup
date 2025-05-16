2204d43bf4181765c8c0c3524bbabd40f6c15a80
Alberto Pedron <albertop2197@gmail.com>
2025-05-17 00:34:03 +0200

    Upgrade AGP to v8.10 and Gradle to v8.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val agpVersion = "8.6.1"
+    val agpVersion = "8.10.0"
     val kotlinVersion = "2.0.21"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
