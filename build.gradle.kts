5196976ec2b3d90ff4a9f004506a71bce5b8eea1
Alberto Pedron <albertop2197@gmail.com>
2025-05-17 02:45:52 +0200

    Update Hilt to v2.56.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.android.library") version agpVersion apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
-    id("com.google.dagger.hilt.android") version "2.53.1" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
     id("com.google.devtools.ksp") version "$kotlinVersion-1.0.32" apply false
     id("org.gradle.android.cache-fix") version "3.0.1" apply false
 }
