e6a383826c7ed91b0dfd58110812557fe5eb96c8
Alberto Pedron <albertop2197@gmail.com>
2025-07-17 20:02:19 +0200

    Do not change statusbar color

--- app/src/main/java/org/alberto97/ouilookup/ui/theme/Theme.kt
+++ app/src/main/java/org/alberto97/ouilookup/ui/theme/Theme.kt
@@ -51,7 +51,6 @@ fun OUILookupTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.surface.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
