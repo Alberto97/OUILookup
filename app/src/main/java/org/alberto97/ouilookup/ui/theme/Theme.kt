c6d43ca9469f5da9f5ca24fdb551554c54e4e6b7
Alberto Pedron <albertop2197@gmail.com>
2025-07-18 13:44:56 +0200

    Revert "Do not change statusbar color"
    This reverts commit e6a383826c7ed91b0dfd58110812557fe5eb96c8.

--- app/src/main/java/org/alberto97/ouilookup/ui/theme/Theme.kt
+++ app/src/main/java/org/alberto97/ouilookup/ui/theme/Theme.kt
@@ -51,6 +51,7 @@ fun OUILookupTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.surface.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
