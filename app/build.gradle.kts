2b4259112b181c3cddc8e155cf9fc37f6878e0f2
Alberto Pedron <albertop2197@gmail.com>
2026-01-18 17:16:58 +0100

    Remove AGP incompatible org.jetbrains.kotlin.android plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
