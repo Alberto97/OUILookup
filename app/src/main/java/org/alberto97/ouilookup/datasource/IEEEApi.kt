91d22c2c49898106eb02df629c7a3cb9cccd16e2
Alberto Pedron <albertop2197@gmail.com>
2026-05-15 21:45:26 +0200

    IEEEApi: Remove User Agent

--- app/src/main/java/org/alberto97/ouilookup/datasource/IEEEApi.kt
+++ app/src/main/java/org/alberto97/ouilookup/datasource/IEEEApi.kt
@@ -2,9 +2,11 @@ package org.alberto97.ouilookup.datasource
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface IEEEApi {
 
+    @Headers("User-Agent: ")
     @GET("oui/oui.csv")
     suspend fun fetchOui(): Response<String>
 }
